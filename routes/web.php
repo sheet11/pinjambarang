9b3ab031200f3c150659a20b01ab10b71cbebfa3
rey <ahmadreynaldo@gmail.com>
2023-08-08 16:10:48 +0700

    css

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,11 @@ route::resource('/posts', \App\Http\Controllers\PostController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
